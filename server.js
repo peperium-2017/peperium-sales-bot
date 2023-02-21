a811931e9d7e3cb6679abd8453689e9aaf971004
crypt0biwan <crypt0biwan@gmail.com>
2023-02-21 11:15:29 +0100

    update

--- server.js
+++ server.js
@@ -9,23 +9,35 @@ const {
 	TWITTER_API_KEY, TWITTER_API_KEY_SECRET, TWITTER_ACCESS_TOKEN_KEY, TWITTER_ACCESS_TOKEN_SECRET
 } = process.env;
 
-const webhookClient = new WebhookClient({id: DISCORD_ID, token: DISCORD_TOKEN});
-const _twitterClient = new TwitterApi({
-	appKey: TWITTER_API_KEY,
-	appSecret: TWITTER_API_KEY_SECRET,
-	accessToken: TWITTER_ACCESS_TOKEN_KEY,
-	accessSecret: TWITTER_ACCESS_TOKEN_SECRET
-});
-const twitterClient = _twitterClient.readWrite;
+const ENABLE_DISCORD = DISCORD_ID !== ''
+const ENABLE_TWITTER = TWITTER_API_KEY !== ''
+
+if(ENABLE_DISCORD) {
+	const webhookClient = new WebhookClient({id: DISCORD_ID, token: DISCORD_TOKEN});
+}
+
+if(ENABLE_TWITTER) {
+	const _twitterClient = new TwitterApi({
+		appKey: TWITTER_API_KEY,
+		appSecret: TWITTER_API_KEY_SECRET,
+		accessToken: TWITTER_ACCESS_TOKEN_KEY,
+		accessSecret: TWITTER_ACCESS_TOKEN_SECRET
+	});
+	const twitterClient = _twitterClient.readWrite;
+}
 
 const transferHandler = async ({ data, totalPrice, buyer, seller, ethPrice, token, platforms }) => {
-	// post to discord
-	const discordMsg = await formatDiscordMessage({ data, totalPrice, buyer, seller, ethPrice, token, platforms });
-	webhookClient.send(discordMsg).catch(console.error);
+	if(ENABLE_DISCORD) {
+		// post to discord
+		const discordMsg = await formatDiscordMessage({ data, totalPrice, buyer, seller, ethPrice, token, platforms });
+		webhookClient.send(discordMsg).catch(console.error);
+	}
 
-	// tweet
-	const [twitterMessage, mediaIds] = await formatTwitterMessage(twitterClient, { data, totalPrice, buyer, seller, ethPrice, token, platforms });
-	twitterClient.v1.tweet(twitterMessage, { media_ids: mediaIds }).catch(console.error);
+	if(ENABLE_TWITTER) {
+		// tweet
+		const [twitterMessage, mediaIds] = await formatTwitterMessage(twitterClient, { data, totalPrice, buyer, seller, ethPrice, token, platforms });
+		twitterClient.v1.tweet(twitterMessage, { media_ids: mediaIds }).catch(console.error);
+	}
 };
 
 console.log("Starting bot");
